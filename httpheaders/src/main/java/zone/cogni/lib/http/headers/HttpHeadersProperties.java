1fe9f84f7da4e414af1aa902495cffc1e341d612
domi.vds <domi.vds@cogni.zone>
2023-01-12 17:12:37 +0100

    Init list of config to avoid NPE when running without config...

--- httpheaders/src/main/java/zone/cogni/lib/http/headers/HttpHeadersProperties.java
+++ httpheaders/src/main/java/zone/cogni/lib/http/headers/HttpHeadersProperties.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class HttpHeadersProperties {
 
-  private List<HttpHeader> headers;
+  private List<HttpHeader> headers = new ArrayList<>();
 
   public enum Type {
     plain
